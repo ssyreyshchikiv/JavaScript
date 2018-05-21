5408c2f18527079eab5ab22be17222f747c9a093
ssyreyshchikov <ssyreyshchikov@mail.ru>
2018-05-21 21:18:34 +0300

    add task 3 HomeWork 9

--- HomeWork9/script.js
+++ HomeWork9/script.js
@@ -35,3 +35,38 @@ function counter() {
 
 elem.addEventListener("click", counter);
 */
+
+// 3*. Изобразить клавиатуру и расположенные 
+// на ней клавиши в виде html документа. 
+// При нажатии клавиши пользователем 
+// на клавиатуре, подсвечивать нажатую 
+// клавишу на клавиатуре изображенной 
+// в html и выводить нажатое значение.
+
+// получаем код нажатой клавиши
+
+	
+
+
+
+document.onkeydown = function checkKeycode(e) {
+	var writeDesk = document.getElementById("write");
+	var li = document.getElementsByClassName("letter");
+	var keycode = e.keyCode;
+	writeDesk.textContent = String.fromCharCode(keycode).toLowerCase();
+
+	for (var i = 0; i < li.length; i++) {
+    	if(li[i].innerText == writeDesk.textContent) {
+    		li[i].style.background="grey";
+    	}
+    }
+
+};
+
+document.onkeyup = function() {
+	var li = document.getElementsByClassName("letter");
+	for (var i = 0; i < li.length; i++) {
+    		li[i].style.background="white";
+    	}
+}
+
